479f3a46c2897d6e7826e35a3c28c7a415a8b8d8
Jentan Bernardus <jentanbernardus@users.noreply.github.com>
2019-11-30 15:08:24 -0400

    tidy gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,37 @@
 'use strict';
 
-// const 	{ src, dest, watch, series, parallel, lastRun } = require('gulp'),
-const 	{ gulp, src, dest, series, parallel} = require('gulp'),
-        htmlmin = require('gulp-htmlmin'),
-        uglify = require('gulp-uglify'),
-        rename = require('gulp-rename'),
-        concat = require('gulp-concat'),
-        imagemin = require('gulp-imagemin'),
-        imageDataURI = require('gulp-image-data-uri'),
-        sass = require('gulp-sass'),
-        minifyCSS = require('gulp-minify-css'),
-        purify = require('gulp-purifycss'),
-        watch = require('gulp-watch'),
-        browserSync = require('browser-sync').create();
-        
+// const  { src, dest, watch, series, parallel, lastRun } = require('gulp'),
+const { gulp, src, dest, series, parallel } = require('gulp'),
+  htmlmin = require('gulp-htmlmin'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  concat = require('gulp-concat'),
+  imagemin = require('gulp-imagemin'),
+  imageDataURI = require('gulp-image-data-uri'),
+  sass = require('gulp-sass'),
+  minifyCSS = require('gulp-minify-css'),
+  purify = require('gulp-purifycss'),
+  watch = require('gulp-watch'),
+  browserSync = require('browser-sync').create();
+
 // paths
 var paths = {
-		html: {
-        src: './*.html',
-        dist: 'dist/'
-    },
-    css: {
-        src: "src/sass/**/*.scss",
-        dist: "dist/css/"
-    },
-    js: {
-        src:'src/js/*.js',
-        dist: 'dist/js/'
-    },
-    img: {
-        src: 'src/img/*',
-        dist: 'dist/img/'
-    }
+  html: {
+    src: './*.html',
+    dist: 'dist/'
+  },
+  css: {
+    src: "src/sass/**/*.scss",
+    dist: "dist/css/"
+  },
+  js: {
+    src: 'src/js/*.js',
+    dist: 'dist/js/'
+  },
+  img: {
+    src: 'src/img/*',
+    dist: 'dist/img/'
+  }
 };
 
 // private task example
@@ -45,12 +45,13 @@ var paths = {
 }*/
 
 // compile sass to css
-function css () {
+function css() {
   return src(paths.css.src)
-    .pipe(sass().on('error',sass.logError))
+    .pipe(sass()
+      .on('error', sass.logError))
     .pipe(purify([paths.js.dist, paths.html.src]))
     .pipe(minifyCSS({
-        keepSpecialComments: 0
+      keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(dest(paths.css.dist))
@@ -61,7 +62,7 @@ exports.css = css;
 // concat + minify scripts
 function js() {
   return src(paths.js.src)
-  	.pipe(concat('scripts.js'))
+    .pipe(concat('scripts.js'))
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(dest(paths.js.dist))
@@ -70,43 +71,43 @@ function js() {
 exports.js = js;
 
 // compress images
-function img () {
-    return src(paths.img.src)
-    	// No options
-    	// .pipe(imagemin())
-    	
-    	// With options
-        .pipe(imagemin([
-        		imagemin.gifsicle({interlaced: true}),
-        		imagemin.jpegtran({progressive: true}),
-        		imagemin.optipng({optimizationLevel: 5}),
+function img() {
+  return src(paths.img.src)
+    // No options
+    // .pipe(imagemin())
+
+    // With options
+    .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
             imagemin.svgo({
-			        plugins: [
-			            {removeViewBox: true},
-			            {cleanupIDs: false}
-			        ]
-			    })
-        	], {
-					    verbose: true
-					}))
-        .pipe(dest(paths.img.dist))
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+              ]
+      })
+          ], {
+      verbose: true
+    }))
+    .pipe(dest(paths.img.dist))
 }
 exports.img = img;
 
 // convert images to data uri
-exports.datauri = function () {
-    return src (paths.img.src)
+exports.datauri = function() {
+  return src(paths.img.src)
     .pipe(imageDataURI())
     // combine css into one file
-    .pipe(concat('data-uri.css')) 
+    .pipe(concat('data-uri.css'))
     .pipe(dest(paths.css.dist));
 }
 
 // minify html
-function html()  {
-	return src('index.html')
-  	.pipe(htmlmin({
-   		collapseWhitespace: true,
+function html() {
+  return src('index.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
       collapseInlineTagWhitespace: true,
       removeComments: true
     }))
@@ -117,19 +118,20 @@ exports.html = html
 
 // browsersync + watch
 function sync() {
-	browserSync.init({
-		server: {
-    	baseDir: "./dist",
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
       index: "/index.html"
     }
   });
   // You can use a single task
   // watch('src/*.css', css);
-  
+
   // Or a composed task
   watch([paths.html.src, paths.js.src, paths.css.src],
-  	parallel(html, css, js)
-  ).on('change', browserSync.reload);
+      parallel(html, css, js)
+    )
+    .on('change', browserSync.reload);
 }
 exports.sync = sync;
 
@@ -137,7 +139,7 @@ exports.sync = sync;
 // exports
 exports.dev = series(html, css, js, sync);
 exports.default = series(
-	html,
+  html,
   parallel(css, js),
   img, sync
 );
